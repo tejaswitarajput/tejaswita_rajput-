f9cdbd3641c987c2a4d512f7a05a78c649b816cf
Tejaswita Rathod <tejaswita.rathod@anant.co.in>
2021-06-30 23:33:08 +0530

    change in edit and save

--- lib/fetures/create_employee_screen.dart
+++ lib/fetures/create_employee_screen.dart
@@ -1,5 +1,7 @@
 import 'package:demo_projec/constants/circular_progressbar_fullscreen.dart';
 import 'package:demo_projec/constants/palette.dart';
+import 'package:demo_projec/constants/route_generator.dart';
+import 'package:demo_projec/constants/router_constants.dart';
 import 'package:demo_projec/constants/snackbar.dart';
 import 'package:demo_projec/providers/employee_provider.dart';
 import 'package:flutter/material.dart';
@@ -29,8 +31,114 @@ class _CreateEmployeeScreenState extends State<CreateEmployeeScreen> {
         child: Scaffold(
           appBar: AppBar(
             centerTitle: true,
-            title: Text('Create Employee'),
-            backgroundColor: Palette.primary,
+            backgroundColor: Colors.blueAccent,
+            toolbarHeight: 100,
+            leading: MaterialButton(
+              onPressed: () {
+                showGeneralDialog(
+                    context: context,
+                    barrierDismissible: true,
+                    barrierLabel: MaterialLocalizations.of(context)
+                        .modalBarrierDismissLabel,
+                    barrierColor: Colors.black45,
+                    transitionDuration: const Duration(milliseconds: 200),
+                    pageBuilder: (BuildContext buildContext,
+                        Animation animation, Animation secondaryAnimation) {
+                      return Center(
+                        child: Container(
+                          width: 300,
+                          height: 200,
+                          padding: EdgeInsets.all(20),
+                          color: Colors.white,
+                          child: Scaffold(
+                            body: Card(
+                                child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: Align(
+                                      alignment: Alignment.topRight,
+                                      child: IconButton(
+                                        icon: new Icon(Icons.close),
+                                        color: Colors.black,
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                      )),
+                                ),
+                                MaterialButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(
+                                        context,
+                                        RouterConstants
+                                            .CreateEmployeeScreenRoute,
+                                        arguments:
+                                            EmployeeArgument(employeeId: ""));
+                                  },
+                                  minWidth: 250.0,
+                                  height: 35,
+                                  color: Colors.blueAccent,
+                                  child: new Text('Create Employee',
+                                      style: new TextStyle(
+                                          fontSize: 16.0, color: Colors.white)),
+                                ),
+                                MaterialButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(context,
+                                        RouterConstants.HomeScreenRoute);
+                                  },
+                                  minWidth: 250.0,
+                                  height: 35,
+                                  color: Colors.blueAccent,
+                                  child: new Text('Employee List',
+                                      style: new TextStyle(
+                                          fontSize: 16.0, color: Colors.white)),
+                                )
+                              ],
+                            )),
+                          ),
+                        ),
+                      );
+                    });
+              },
+              child: Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+            ),
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SizedBox(height: 20),
+                  CircleAvatar(
+                      backgroundImage: AssetImage('assets/icons/logo.png'),
+                      radius: 20),
+                  MaterialButton(
+                    onPressed: () {},
+                    child: Icon(Icons.search, color: Colors.white),
+                  ),
+                  MaterialButton(
+                    onPressed: () {},
+                    child:
+                        Icon(Icons.account_circle_rounded, color: Colors.white),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Conaug",
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                      Text(
+                        "Flutter",
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
           ),
           body: Stack(
             alignment: Alignment.topCenter,
